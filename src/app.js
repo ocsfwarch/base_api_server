c257b3fea5f971fec80bbc617945e1f9a7dae032
ocsfwarch <38299239+ocsfwarch@users.noreply.github.com>
2021-06-08 10:48:59 -0700

    Adds basic test route to the server.

--- src/app.js
+++ src/app.js
@@ -5,18 +5,16 @@ require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 const express = require("express");
 const cors = require("cors");
 
+const testRouter = require("./components/TestRoute/test.router");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
-//const reservationsRouter = require("./reservations/reservations.router");
-//const tablesRouter = require("./tables/tables.router");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-//app.use("/reservations", reservationsRouter);
-//app.use("/tables", tablesRouter);
+app.use("/test", testRouter);
 
 app.use(notFound);
 app.use(errorHandler);
