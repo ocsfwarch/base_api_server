2ac9c85a69877364944c4872f2e089c022c02bad
ocsfwarch <38299239+ocsfwarch@users.noreply.github.com>
2021-06-28 17:15:45 -0700

    Update naming for easier identification of functionality

--- src/components/TestRoute/test.router.js
+++ src/components/TestRoute/test.router.js
@@ -6,10 +6,10 @@
  * test route for connectivity verification.
  */
 
-const router = require("express").Router();
+const testRouter = require("express").Router();
 const controller = require("./test.controller");
 const methodNotAllowed = require("../../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).all(methodNotAllowed);
+testRouter.route("/").get(controller.list).all(methodNotAllowed);
 
-module.exports = router;
+module.exports = testRouter;
